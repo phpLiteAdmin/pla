5b7a6c302c88b8269216c1c0a206bcea36731615
Naveen <mailtomassnaveen@gmail.com>
2017-07-21 22:22:14 +0530

    Add isNumber function for restrict to user type only number

--- resources/phpliteadmin.js
+++ resources/phpliteadmin.js
@@ -237,3 +237,11 @@ function checkFileSize(input)
 	}
 	return true;
 }
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
