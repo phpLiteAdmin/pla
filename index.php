2dba6d1700296ef3f8e565e926fc8355c0f017a0
Christopher Kramer <crazy4chrissi@gmail.com>
2021-07-08 11:06:25 +0100

    language attribute of html tag reflects the chosen language. Fixes https://bitbucket.org/phpliteadmin/public/issues/396/html-tag-language-settings-need-to-be

--- index.php
+++ index.php
@@ -1448,7 +1448,7 @@ ob_end_clean();
 header('Content-Type: text/html; charset=utf-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>">
 <head>
 <!-- Copyright <?php echo date("Y").' '.PROJECT.' ('.PROJECT_URL.')'; ?> -->
 <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
