75303bea061bfb6abc27fd0a53cc0cb2bdf74327
Christopher Kramer <crazy4chrissi@gmail.com>
2021-07-09 10:30:07 +0100

    Print a useful error message when session_start() failed. Solves this issue: https://bitbucket.org/phpliteadmin/public/issues/384/csrf-token-is-wrong-please-try-to-login

--- index.php
+++ index.php
@@ -56,7 +56,10 @@ if (isset($_GET['resource']))
 
 // don't mess with this - required for the login session
 ini_set('session.cookie_httponly', '1');
-session_start();
+if(!session_start())
+{
+	die("Could not start a new session. Check your php setup regarding sessions.");
+}
 
 // version-number added so after updating, old session-data is not used anylonger
 // cookies names cannot contain symbols, except underscores
